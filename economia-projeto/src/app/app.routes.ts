7f794153b7b466163bc18c94366cf33858476e38
Lucas Gabriel Cardoso Souza <lucasgabrielcs68@gmail.com>
2024-12-06 18:19:19 -0300

    bootstrap inserido junto com a tela inicial

--- economia-projeto/src/app/app.routes.ts
+++ economia-projeto/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { FormularioComponent } from './formulario/formulario.component';
 import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 
 export const routes: Routes = [
  {
     path: '',
+    component: PaginaInicialComponent
+ }, 
+ {
+    path: 'formulario',
    component: FormularioComponent
  },
  {
